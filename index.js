101b53f6ec5188ff14d1abf8e793f4c1f48f4a03
AasthaDixit26 <aaasthadixit@gmail.com>
2022-12-03 00:48:47 +0530

    This project is for the practioners like me so that we can be more confident in ourselves and also a little fun.

---
+++ index.js
@@ -0,0 +1,14 @@
+function glowoff(){
+    let v=document.getElementById("bulb")
+    v.src="off1.jpg";
+    // let p=document.getElementById("bulb")
+    // p.style.width="420px";
+}
+function glowon(){
+    let v=document.getElementById("bulb")
+    v.src="on1.jpg";
+    // let p=document.getElementById("bulb")
+    // p.style.width="400px";
+}
+
+
